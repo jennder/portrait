93bbffd387d5078313b24c8ff4cb98b559898070
Jenn Der <der.je@husky.neu.edu>
2020-12-06 15:15:26 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
